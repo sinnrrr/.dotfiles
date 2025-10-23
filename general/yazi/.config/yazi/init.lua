67353d06d803d53210e475cab449745ff409c493
Dmytro Soltysiuk <dimasoltusyuk@gmail.com>
2025-10-23 16:36:54 -0700

    yazi dependency upgrade

--- general/yazi/.config/yazi/init.lua
+++ general/yazi/.config/yazi/init.lua
@@ -1,7 +1,8 @@
 -- require("full-border"):setup()
--- require("no-status"):setup()
-require("git"):setup()
-require("recycle-bin"):setup()
 -- require("session"):setup {
 -- 	sync_yanked = true,
 -- }
+
+require("git"):setup()
+require("no-status"):setup()
+require("recycle-bin"):setup()

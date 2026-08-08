5454e6c7a1a774420004f0ff938a2dd28c1db271
Dmytro Soltysiuk <dimasoltusyuk@gmail.com>
2026-08-08 14:13:26 -0700

    shift tab ctrl-shift vim

--- general/wezterm/.config/wezterm/wezterm.lua
+++ general/wezterm/.config/wezterm/wezterm.lua
@@ -60,6 +60,26 @@ config.keys = {
 		mods = "CTRL",
 		action = act.ActivateTabRelative(1),
 	},
+	{
+		key = "LeftArrow",
+		mods = "CMD|SHIFT",
+		action = act.MoveTabRelative(-1),
+	},
+	{
+		key = "RightArrow",
+		mods = "CMD|SHIFT",
+		action = act.MoveTabRelative(1),
+	},
+	{
+		key = "h",
+		mods = "CTRL|SHIFT",
+		action = act.MoveTabRelative(-1),
+	},
+	{
+		key = "l",
+		mods = "CTRL|SHIFT",
+		action = act.MoveTabRelative(1),
+	},
 }
 
 return config
